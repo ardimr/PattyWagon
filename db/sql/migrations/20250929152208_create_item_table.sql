8abfe2a1f2c5506ad26df372e7cffa701b82b995
ardimr <rizkyardimaulana@gmail.com>
2025-10-04 18:47:24 +0700

    mod: adden missing necessary fields

--- db/sql/migrations/20250929152208_create_item_table.sql
+++ db/sql/migrations/20250929152208_create_item_table.sql
@@ -4,7 +4,9 @@ CREATE TABLE items (
   id BIGSERIAL PRIMARY KEY,
   merchant_id BIGINT NOT NULL,
   name VARCHAR(255) NOT NULL,
-  category VARCHAR(255),
+  category VARCHAR(50) NOT NULL,
+  price NUMERIC(12,2) NOT NULL,
+  image_url TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW(),
   CONSTRAINT fk_merchant
