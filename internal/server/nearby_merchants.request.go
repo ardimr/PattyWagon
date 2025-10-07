5dceac59faf69cb939b9516e113c22501f5ea5c6
Rizky Ardi Maulana <rizkyardimaulana@gmail.com>
2025-10-07 21:43:55 +0700

    Feat/purchase (#11)
    * feat: add coordinates extractor utils
    
    * feat: add EstimateOrderPrice
    
    * feat: added mock repository and mock location service for testing purpose
    
    * feat: find nearby merchants
    
    * feat: implement strategy to list merchants from database
    
    * chore: add merchant location model
    
    * refactor: split the strategy into 2, by resolution and k-ring
    
    * feat: implemented multiple phase of finding nearby merchants
    
    * feat: post admin merchant endpoint api
    
    * feat: get admin merchant endpoint api
    
    * feat: post item endpoint api
    
    * chore: add check existing merchant on create items process
    
    * feat: add get items endpoint api with filter & pagination
    
    * chore: re-structure get merchants respown & add meta response
    
    * feat: implemented nearby merchants repository and added tester
    
    * feat: populate merchant locations
    
    * feat: implemented concurrent query
    
    * fix: merchant post validation
    
    * mod: adden missing necessary fields
    
    * fix: resolve model conflicts
    
    * mod: add external port 35432 for db
    
    * chore: add db-reset
    
    * fix: added imageUrl validation and fix merchant detail response
    
    * fix: items creation and retrieval
    
    * fix: query without cell as params
    
    * chore: rename neraby_merchants feature related files
    
    ---------
    
    Co-authored-by: Abimanyu Luthfi Rizq Ramadhan <abimanyu177@gmail.com>
    Co-authored-by: aliflazuardi <aliflazuardi250999@gmail.com>

---
+++ internal/server/nearby_merchants.request.go
@@ -0,0 +1,101 @@
+package server
+
+import (
+	"PattyWagon/internal/model"
+	"PattyWagon/internal/utils"
+)
+
+type OrderEstimationRequest struct {
+	UserLocation LocationRequest `json:"userLocation" validate:"required"`
+	Orders       []OrderRequest  `json:"orders"`
+}
+
+type LocationRequest struct {
+	Lat  float64 `json:"lat" validate:"required,latitude"`
+	Long float64 `json:"long" validate:"required,longitude"`
+}
+
+type OrderRequest struct {
+	MerchantID      string             `json:"merchantId" validate:"required"`
+	IsStartingPoint bool               `json:"isStartingPoint" validate:"required"`
+	Items           []OrderItemRequest `json:"items" validate:"required"`
+}
+
+type OrderItemRequest struct {
+	ItemID   string `json:"itemId" validate:"required"`
+	Quantity int    `json:"quantity" validate:"required,gt=0"`
+}
+
+type FindNearbyMerchantRequest struct {
+	UserLocation     LocationRequest
+	MerchantID       string `query:"merchantId"`
+	Limit            string `query:"limit"`
+	Offset           string `query:"offset"`
+	Name             string `query:"name"`
+	MerchantCategory string `query:"merchantCategory"`
+}
+
+func (r *OrderItemRequest) ToModel() model.OrderItem {
+	return model.OrderItem{
+		ItemID:   utils.String2Int64(r.ItemID, 0),
+		Quantity: r.Quantity,
+	}
+}
+
+func (r *LocationRequest) ToModel() model.Location {
+	return model.Location{
+		Lat:  r.Lat,
+		Long: r.Long,
+	}
+}
+
+func (r *OrderRequest) ToModel() model.Order {
+	merchantId := utils.String2Int64(r.MerchantID, 0)
+
+	var orderItems []model.OrderItem
+	for _, item := range r.Items {
+		orderItems = append(orderItems, item.ToModel())
+	}
+
+	return model.Order{
+		MerchantID:      merchantId,
+		IsStartingPoint: r.IsStartingPoint,
+		Items:           orderItems,
+	}
+}
+
+func (r *OrderEstimationRequest) ToModel() model.OrderEstimation {
+	var res model.OrderEstimation
+	var orders []model.Order
+
+	for _, order := range r.Orders {
+		orders = append(orders, order.ToModel())
+	}
+
+	res.UserLocation = r.UserLocation.ToModel()
+	res.Orders = orders
+	return res
+}
+
+func (r *FindNearbyMerchantRequest) ToModel() model.FindNerbyMerchantParams {
+	var params model.FindNerbyMerchantParams
+
+	if r.MerchantID != "" {
+		parsedMerchantID := utils.String2Int64(r.MerchantID, 0)
+		params.MerchantID = &parsedMerchantID
+	}
+
+	params.Limit = utils.String2Int32(r.Limit, 5)
+
+	params.Offset = utils.String2Int32(r.Offset, 0)
+
+	if r.Name != "" {
+		params.Name = &r.Name
+	}
+
+	if r.MerchantCategory != "" {
+		params.MerchantCategory = &r.MerchantCategory
+	}
+
+	return params
+}
---
+++ internal/server/nearby_merchants.response.go
@@ -0,0 +1,122 @@
+package server
+
+import (
+	"PattyWagon/internal/model"
+	"strconv"
+	"time"
+)
+
+type EstimationPriceResponse struct {
+	CalculateEstimateID        string  `json:"calculateEstimateId"`
+	TotalPrice                 float64 `json:"totalPrice"`
+	EstimatedDeliveryInMinutes int64   `json:"estimatedDeliveryInMinutes"`
+}
+
+func (r *EstimationPriceResponse) FromModel(input model.EstimationPrice) {
+	r.CalculateEstimateID = strconv.Itoa(int(input.ID))
+	r.EstimatedDeliveryInMinutes = input.EstimatedDeliveryInMinutes
+	r.TotalPrice = input.TotalPrice
+}
+
+func NewEstimationPriceResponse(input model.EstimationPrice) EstimationPriceResponse {
+	return EstimationPriceResponse{
+		CalculateEstimateID:        strconv.Itoa(int(input.ID)),
+		TotalPrice:                 input.TotalPrice,
+		EstimatedDeliveryInMinutes: input.EstimatedDeliveryInMinutes,
+	}
+}
+
+type LocationResponse struct {
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
+}
+
+type Merchant struct {
+	MerchantID       string           `json:"merchantId"`
+	Name             string           `json:"name"`
+	MerchantCategory string           `json:"merchantCategory"`
+	ImageUrl         string           `json:"imageUrl"`
+	Location         LocationResponse `json:"location"`
+	CreatedAt        time.Time        `json:"createdAt"`
+}
+
+type Item struct {
+	ItemID          string    `json:"itemId"`
+	Name            string    `json:"name"`
+	ProductCategory string    `json:"productCategory"`
+	Price           float64   `json:"price"`
+	ImageUrl        string    `json:"imageUrl"`
+	CreatedAt       time.Time `json:"createdAt"`
+}
+
+type MerchantWithItem struct {
+	Merchant Merchant `json:"merchant"`
+	Items    []Item   `json:"item"`
+}
+
+type FindNearbyMerchantsResponseMeta struct {
+	Limit  int `json:"limit"`
+	Offset int `json:"offset"`
+	Total  int `json:"total"`
+}
+type FindNearbyMerchantsResponse struct {
+	Data []MerchantWithItem              `json:"data"`
+	Meta FindNearbyMerchantsResponseMeta `json:"meta"`
+}
+
+func NewLocationResponse(lat, long float64) LocationResponse {
+	return LocationResponse{
+		Lat:  lat,
+		Long: long,
+	}
+}
+
+func NewMerchantResponse(input model.Merchant) Merchant {
+	return Merchant{
+		MerchantID:       strconv.Itoa(int(input.ID)),
+		Location:         NewLocationResponse(input.Latitude, input.Longitude),
+		Name:             input.Name,
+		MerchantCategory: *input.Category,
+		ImageUrl:         input.ImageURL,
+		CreatedAt:        input.CreatedAt,
+	}
+}
+
+func NewItemResponse(input model.Item) Item {
+	return Item{
+		ItemID:          strconv.Itoa(int(input.ID)),
+		Name:            input.Name,
+		ProductCategory: input.Category,
+		Price:           input.Price,
+		ImageUrl:        input.ImageURL,
+		CreatedAt:       input.CreatedAt,
+	}
+}
+
+func NewMultipleItemsResponse(inputs []model.Item) []Item {
+	var items []Item
+	for _, item := range inputs {
+		items = append(items, NewItemResponse(item))
+	}
+	return items
+}
+
+func NewMerchantWithItem(input model.MerchantItem) MerchantWithItem {
+	return MerchantWithItem{
+		Merchant: NewMerchantResponse(input.Merchant),
+		Items:    NewMultipleItemsResponse(input.Items),
+	}
+}
+
+func NewFindNearbyMerchantsResponse(inputs []model.MerchantItem, meta FindNearbyMerchantsResponseMeta) FindNearbyMerchantsResponse {
+	var merchantWithItems []MerchantWithItem
+
+	for _, input := range inputs {
+		merchantWithItems = append(merchantWithItems, NewMerchantWithItem(input))
+	}
+	return FindNearbyMerchantsResponse{
+		Data: merchantWithItems,
+		Meta: meta,
+	}
+
+}
